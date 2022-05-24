01d51d2a3f7d5a9d67d96891bd138396ff4462e6
Souhaïb Azzouz <sou_sou_azzouz@hotmail.com>
2022-05-24 17:17:46 +0200

    Premier essai Login

--- client/src/components/LoginForm/LoginForm.js
+++ client/src/components/LoginForm/LoginForm.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import Axios from 'axios'
+import { useState } from 'react';
 
 
 const LoginForm = () => {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    /*const register = () => {
+        Axios.post('http://localhost:3001/register', {
+            username: usernameReg,
+            password: passwordReg,
+        }).then((response) => {
+            console.log(response)
+        })
+    }*/
+
+    const login = () => {
+        Axios.post('http://localhost:3001/login', {
+            username: username,
+            password: password,
+        }).then((response) => {
+
+            if (response.data.message) {
+                setloginStatut(response.data.message)
+            } else {
+                setloginStatut(response.data[0].username);
+                console.log(response.data)
+            }
+        })
+    }
+
+    const [loginStatut, setloginStatut] = useState('');
+
     return (
         <>
             <div className="container">
@@ -16,11 +48,25 @@ const LoginForm = () => {
                                     <div className="services__widget-content">
                                         <div className="services__form">
                                             <form>
-                                                <input type="email" placeholder="Email" />
-                                                <input type="password" placeholder="Mot de Passe" />
-                                                <button className="z-btn z-btn-3 w-100">Connexion</button>
+                                                <input
+                                                    type="text"
+                                                    onChange={(e) => {
+                                                        setUsername(e.target.value);
+                                                    }}
+                                                    placeholder="Username" />
+                                                <input
+                                                    type="password"
+                                                    onChange={(e) => {
+                                                        setPassword(e.target.value);
+                                                    }}
+                                                    placeholder="Mot de Passe" />
+                                                <button
+                                                    type='button'
+                                                    className="z-btn z-btn-3 w-100"
+                                                    onClick={login}>Connexion</button>
                                             </form>
                                         </div>
+                                        <h4>{loginStatut}</h4>
                                     </div>
                                 </div>
                             </div>
--- serveur/index.js
+++ serveur/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mysql = require('mysql');
-
+const cors = require("cors");
 
 const app = express();
+
+app.use(express.json());
+app.use(cors());
 
 const db = mysql.createConnection({
     user: "root",
@@ -11,7 +14,44 @@ const db = mysql.createConnection({
     database: "tfe_riwa"
 });
 
-app.use(express.json());
+app.post('/register', (req, res) => {
+
+    const username = req.body.username
+    const password = req.body.password
+
+    db.query(
+        'INSERT INTO admin (username, password) VALUE (?,?)',
+        [username, password],
+        (err, result) => {
+            console.log(err)
+        })
+})
+
+
+app.post('/login', (req, res) => {
+
+    const username = req.body.username
+    const password = req.body.password
+
+    db.query(
+        'SELECT * FROM admin WHERE username = ? AND password = ?',
+        [username, password],
+        (err, result) => {
+
+            if (err) {
+                res.send({ err: err })
+            }
+            else {
+                if (result.length > 0) {
+                    res.send(result)
+                } else {
+                    res.send({ message: "Veuillez entrer un nom d'utilisateur et un mot de passe valide!" });
+                }
+            }
+        })
+})
+
+
 
 app.get('/', (req, res) => {
     res.send("Hello Souhaïb");
