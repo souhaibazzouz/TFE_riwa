0e6256284bc3a55717e3546f7b5824dea72a6f73
Souhaïb Azzouz <sou_sou_azzouz@hotmail.com>
2022-06-14 14:09:19 +0200

    test dépoiement vps

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const jwt = require('jsonwebtoken');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
