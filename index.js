ddde4ef039edc02522e161395a65f69455929009
Souhaïb Azzouz <sou_sou_azzouz@hotmail.com>
2022-06-15 01:04:43 +0200

    test dépoiement vps

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://riwa-ac.be");
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
